a2a934bdfd40530778793b370dcfb0e9a1058492
Michael Bortlik <Michael.Bortlik@gmx.de>
2013-04-27 19:21:03 +0200

    commit commit

--- src/main.java
+++ src/main.java
@@ -8,9 +8,7 @@ public class main {
 		// TODO Auto-generated method stub
 		System.out.println("hihi");
 		System.out.println("commit");
-		System.out.println("commit2");
 
-		System.out.println("commit");
 	}
 
 }
